0adcf1e8b2af4137f528c08b1db1570bf77c78ef
HZH <hzhswyz@163.com>
2018-11-28 19:40:59 +0800

    GUI界面V8

--- src/main/java/hzh/web/controller/SkatingController.java
+++ src/main/java/hzh/web/controller/SkatingController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/Skating")
@@ -15,11 +16,13 @@ public class SkatingController {
     private static Logger logger = Logger.getLogger(SkatingController.class);
 
     @RequestMapping("/requestmodel")
-    public ResponseEntity<ConnectResponse> requestmodel(){
+    public ResponseEntity<ConnectResponse> requestmodel(@RequestParam("fileuri") String fileuri){
+        logger.debug("FileURI:"+fileuri);
         ConnectResponse connectResponse = new ConnectResponse();
         connectResponse.setStatus(ConnectionResult.Success);
         connectResponse.setSuccess(true);
         connectResponse.setMessagequeue(new ConnectResponse.Messagequeue("failover://tcp://120.79.16.31:61616","hzh","Hzh1997","queue","Real time detection origin video _ 0"));
         return new ResponseEntity<ConnectResponse>(connectResponse, HttpStatus.OK);
     }
+
 }
